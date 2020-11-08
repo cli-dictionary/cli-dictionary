70554a781f75cc77a594884b7b3f46aabba7a2c7
ropoko <rodrigostramantinoli@gmail.com>
2020-11-08 13:21:23 -0300

    snapcraft avaiable

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='cli-dictionary',
+    version='1.0',
+    description='cli-dictionary',
+    author='Ropoko',
+    author_email='rodrigostramantinoli@gmail.com ',
+    scripts=['bin/cli-dictionary'],
+)
