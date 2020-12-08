5125eaa3e31472c9eede9b305bf972ff512c0b76
ropoko <rodrigostramantinoli@gmail.com>
2020-12-07 23:10:39 -0300

    general changes

--- dictionary/__main__.py
+++ dictionary/__main__.py
@@ -3,57 +3,43 @@ from urllib.request import urlopen
 from languages.language import language
 
 def main():
-    # receives word to find and the word's language
-    # word_lang = str(input(''))
+    word_lang = str(input(''))
 
-    # word = word_lang.split(' ')[0]
-    # lang = word_lang.split(' ')[1]
+    word = word_lang.split(' ')[0].encode('utf-8')
+    lang = word_lang.split(' ')[1].upper()
 
-    # url = ''
-    
-    print(language['EN'])
+    if lang in language:
+        url = language[lang] + '/' + word.decode('utf-8')
+        meaning(url.encode('utf-8'))
+    else:
+        print("""
+            select a valid language:
+            en <english> | pt <portuguese>
+            hi <hindi>   | es <spanish>
+            fr <french>  | ja <japanese>
+            ru <russian> | de <german>
+            it <italian> | ko <korean>
+            zh <chinese> | ar <arabic>
+            tr <turkish>
+        """)
+
+def meaning(url):
+    response = urlopen(url)
+    data = json.loads(response.read())
+
+    for obj in data:
+        try:
+            for i in range(10):
+                if i == 0:
+                    pass
+                else:
+                    print(
+                        str(obj).split("definition':")[i].split('.')[0].replace(
+                            "'",
+                            str(i) + '. '))
+
+        except IndexError as ex:
+            break
 
 if __name__ == '__main__':
     main()   
-
-# if lang.__eq__('en'):
-#     url = 'https://api.dictionaryapi.dev/api/v2/entries/{0}/{1}'.format(
-#         'en', word)
-# elif lang.__eq__('pt'):
-#     url = 'https://api.dictionaryapi.dev/api/v2/entries/{0}/{1}'.format('pt-BR', word)
-# else:
-#     print("""
-#         select a valid language:
-#         en <english>,
-#         pt <portuguese>,
-#         hi <hindi>, 
-#         es <spanish>,
-#         fr <french>,
-#         ja <japanese>,
-#         ru <russian>,
-#         de <german>,
-#         it <italian>,
-#         ko <korean>,
-#         zh <chinese>, 
-#         ar <arabic>,
-#         tr <turkish>
-#     """)
-
-#####################aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-
-# response = urlopen(url)
-# data = json.loads(response.read())
-
-# for obj in data:
-#     try:
-#         for i in range(10):
-#             if i == 0:
-#                 pass
-#             else:
-#                 print(
-#                     str(obj).split("definition':")[i].split('.')[0].replace(
-#                         "'",
-#                         str(i) + '. '))
-
-#     except IndexError as ex:
-#         break
